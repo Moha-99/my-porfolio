6a63186fb11bbdfc0d40ac50b8fff76c6bb9bfd0
Moha-99 <master27599@gmail.com>
2020-10-03 16:25:34 +0200

    Edit style.css

--- js/main.js
+++ js/main.js
@@ -56,3 +56,27 @@ window.onload = function () {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+// Get the current year for the copyright
+$("#year").text(new Date().getFullYear());
+
+// Init Scrollspy
+$("body").scrollspy({ target: "#main-nav" });
+
+// Smooth Scrolling
+$("#main-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
